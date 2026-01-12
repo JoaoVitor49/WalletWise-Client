5d40d69568f7f9751caf43de2b31fe2397ef3672
João Vitor Pimentel <joao.pimentel2017@outlook.com>
2026-01-12 16:01:41 -0300

    create login and signup pages with user models

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './core/layouts/auth-layout.component/auth-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AuthLayoutComponent, 
+        children:[
+            {path: '', redirectTo: 'login', pathMatch: 'full'},
+            {path: 'login', loadComponent: () => import('./features/auth/login/login.component/login.component').then(m => m.LoginComponent)},
+            {path: 'signup', loadComponent: () => import('./features/auth/signUp/sign-up.component/sign-up.component').then(m => m.SignUpComponent)}
+        ]
+    }
+];
